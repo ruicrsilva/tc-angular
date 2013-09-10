4d5b17ddc21bdd79e3ba2f490799c6c390f1b62b
Carl Craig <carlcraig@3c-studios.com>
2013-09-10 17:05:52 +0100

    updated bump task

--- Gruntfile.js
+++ Gruntfile.js
@@ -296,17 +296,7 @@ module.exports = function( grunt ) {
           'package.json',
           'bower.json'
         ],
-        updateConfigs: [],
-        commit: true,
-        commitMessage: 'Release v%VERSION%',
-        commitFiles: [
-          'package.json',
-          'bower.json'
-        ],
-        createTag: true,
-        tagName: 'v%VERSION%',
-        tagMessage: 'Version %VERSION%',
-        push: false
+        updateConfigs: []
       }
     }
 
